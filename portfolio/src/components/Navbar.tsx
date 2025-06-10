5e22da79c26614717d57f889465fd357b2a4aaa1
Ilija Stojev <ilestojev@gmail.com>
2025-06-10 14:15:31 +0200

    Started portfolio navbar

---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white text-lg font-bold">Ilija Stojev</div>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#about" className="text-gray-300 hover:text-white">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="text-gray-300 hover:text-white">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="text-gray-300 hover:text-white">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
